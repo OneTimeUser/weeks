9f9f80d571b6bbd941ae6af975ac22ff07cc6c26
aston <jasonaston@gmail.com>
2014-11-07 17:20:40 -0500

    layout changes to match mockup

--- wp-content/themes/weeks/sidebar.php
+++ wp-content/themes/weeks/sidebar.php
@@ -1,15 +1,16 @@
 <?php
 /**
- * The sidebar containing the main widget area.
+ * The sidebar containing the sub menu area.
  *
  * @package weeks
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
+// if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+// 	return;
+// }
 ?>
 
-<div id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+<div id="secondary" class="submenu-area" role="complementary">
+<!-- 	<?php dynamic_sidebar( 'sidebar-1' ); ?>-->
+	<h3>Whats up submenu!</h3>
 </div><!-- #secondary -->
