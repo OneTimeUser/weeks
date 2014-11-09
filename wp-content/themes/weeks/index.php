309aa338724e27a5d8fc40346404a7961eff6388
Mihai Regos <mihairegos@gmail.com>
2014-11-09 14:27:33 -0500

    Revert 9f9f80d..fcf764c
    This rolls back to commit 9f9f80d571b6bbd941ae6af975ac22ff07cc6c26.

--- wp-content/themes/weeks/index.php
+++ wp-content/themes/weeks/index.php
@@ -13,6 +13,8 @@
 	
 
 get_header(); ?>
+<?php get_sidebar(); ?>
+
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
@@ -43,5 +45,4 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
