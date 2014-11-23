744834a91126d575c7d1238e2b394ce87fb85c2c
aston <jasonaston@gmail.com>
2014-11-23 14:30:41 -0500

    eliminate post information

--- wp-content/themes/weeks/content.php
+++ wp-content/themes/weeks/content.php
@@ -10,7 +10,7 @@
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
-			<?php weeks_posted_on(); ?>
+			<!--<?php weeks_posted_on(); ?>-->
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
@@ -33,6 +33,6 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php weeks_entry_footer(); ?>
+		<!--<?php weeks_entry_footer(); ?>-->
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
