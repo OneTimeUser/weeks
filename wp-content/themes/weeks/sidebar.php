fa3ad824cb05631c3adc3cb2b1368edb3e102b6f
aston <jasonaston@gmail.com>
2014-11-23 15:34:13 -0500

    Merge branch 'categories'

--- wp-content/themes/weeks/sidebar.php
+++ wp-content/themes/weeks/sidebar.php
@@ -12,5 +12,34 @@
 
 <div id="secondary" class="submenu-area" role="complementary">
 <!-- 	<?php dynamic_sidebar( 'sidebar-1' ); ?>-->
-	<h3>Category Section visible only while WORK menu is active, otherwise hidden</h3>
+	<?php 
+	    $args = array(
+		'show_option_all'    => 'All <br>',
+		'orderby'            => 'name',
+		'order'              => 'ASC',
+		'style'              => 'none',
+		'show_count'         => 0,
+		'hide_empty'         => 0,
+		'use_desc_for_title' => 1,
+		'child_of'           => 0,
+		'feed'               => '',
+		'feed_type'          => '',
+		'feed_image'         => '',
+		'exclude'            => '',
+		'exclude_tree'       => '',
+		'include'            => '',
+		'hierarchical'       => 1,
+		'title_li'           => __( 'Categories' ),
+		'show_option_none'   => __( 'No categories' ),
+		'number'             => null,
+		'echo'               => 1,
+		'depth'              => 0,
+		'current_category'   => 0,
+		'pad_counts'         => 0,
+		'taxonomy'           => 'category',
+		'walker'             => null
+	    );
+
+	wp_list_categories( $args ); ?> 
+	
 </div><!-- #secondary -->
